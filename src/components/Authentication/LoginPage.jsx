a718ad51e06b5897ac413f522745aef64db2472f
Moon Young Doon <jo94hk@gamil.com>
2024-06-26 10:33:28 +0900

    로그인폼

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -1,17 +1,28 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./LoginPage.css";
 
 const LoginPage = () => {
-  const passwordRef = useRef(null);
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+    setUser({ email: "", password: "" }); //초기화 하는것
+  };
   return (
     <section className="align_center form_page">
-      <form className="authentication_form">
+      <form onSubmit={handleSubmit} className="authentication_form">
         <h2>로그인 폼</h2>
         <div className="form_inputs">
           <div>
             <label htmlFor="email">Email</label>
             <input
               type="email"
+              value={user.email}
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
               id="email"
               className="form_text_input"
               placeholder="이메일 입력..."
@@ -20,24 +31,13 @@ const LoginPage = () => {
           <div>
             <label htmlFor="password">Password</label>
             <input
-              ref={passwordRef}
               type="password"
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
               id="password"
               className="form_text_input"
               placeholder="패스워드"
             />
-            <button
-              type="button"
-              onClick={() => (passwordRef.current.type = "password")}
-            >
-              비밀번호 숨기기
-            </button>
-            <button
-              type="button"
-              onClick={() => (passwordRef.current.type = "text")}
-            >
-              비밀번호 보이게
-            </button>
           </div>
 
           <button type="submit" className="search_button form_submit">
