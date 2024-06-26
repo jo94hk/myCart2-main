59c012c4e07310449a639a1ce4c0ec77106748c9
Moon Young Doon <jo94hk@gamil.com>
2024-06-26 11:35:00 +0900

    이미지 업로드

--- src/components/Authentication/SignupPage.jsx
+++ src/components/Authentication/SignupPage.jsx
@@ -2,9 +2,12 @@ import { useForm } from "react-hook-form";
 
 import "./SignupPage.css";
 import user from "../../assets/user.webp";
+import { useState } from "react";
 
 const SignupPage = () => {
-  // const [profilePic, setProfilePic] = useState(null);
+  //업로드 파일(이미지파일)
+  const [profilePic, setProfilePic] = useState(null);
+  console.log(profilePic);
   const {
     register,
     handleSubmit,
@@ -26,12 +29,20 @@ const SignupPage = () => {
 
         <div className="image_input_section">
           <div className="image_preview">
-            <img src={user} id="file-ip-1-preview" />
+            <img
+              src={profilePic ? URL.createObjectURL(profilePic) : user}
+              id="file-ip-1-preview"
+            />
           </div>
           <label htmlFor="file-ip-1" className="image_label">
             이미지 업로드
           </label>
-          <input type="file" id="file-ip-1" className="image_input" />
+          <input
+            type="file"
+            onChange={(e) => setProfilePic(e.target.files[0])}
+            id="file-ip-1"
+            className="image_input"
+          />
         </div>
 
         {/* Form Inputs */}
